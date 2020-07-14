e3ef8e6842339f774c3dbf70d88506bea418a170
Pavi532 <udara.sync@gmail.com>
2020-07-14 21:06:53 +0530

    Updates

---
+++ precache-manifest.3403dfc6b6e8dec412eda618fb224624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e671d3edd9215f7b1f7bd76b14193333",
+    "url": "/Corona-React/index.html"
+  },
+  {
+    "revision": "0abe224801934131c176",
+    "url": "/Corona-React/static/css/main.6168683a.chunk.css"
+  },
+  {
+    "revision": "4982750d709ee1bc4369",
+    "url": "/Corona-React/static/js/2.8b21a69d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-React/static/js/2.8b21a69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abe224801934131c176",
+    "url": "/Corona-React/static/js/main.c8ee6d8b.chunk.js"
+  },
+  {
+    "revision": "372dc77d2dd48acefcb5",
+    "url": "/Corona-React/static/js/runtime-main.44ba2eeb.js"
+  }
+]);
